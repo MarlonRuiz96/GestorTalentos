e513ef1d45eaa1e61fcb830c3c5247dfbfcf75b5
Marlon Ruiz <mruiz996@outlook.com>
2023-11-20 00:04:31 -0600

    imagenes agregadas

--- application/views/Pruebas/Temperamento.php
+++ application/views/Pruebas/Temperamento.php
@@ -26,34 +26,83 @@
         <main>
             <div class="d-flex justify-content-start">
                 <img src="<?php echo base_url('assets/img/logo.jpeg'); ?>" class="d-block" alt="Imagen"
-                    style="width: 50%; height: auto;"><br><br>
+                    style="width: 35%; height: auto;"><br><br>
                 <p class="text-center"><br><br>A continuación, deberás elegir una de las cuatro alternativas que se te
                     presentan en cada pantalla. Toma en cuenta que no podrás regresar a corregir. Debes tomar tu tiempo
                     para elegir la mejor opción. Esta prueba, no tiene tiempo. Procura trabajar ordenadamente. Si tienes
                     problemas en ingresar a cualquier página haz click aquí y se te apoyará.
-                <br><br><br>
-                <h2>Llevas <?php echo $Formulario->idRespuesta; ?> de 40:</h2> </p>
-                    
-                    
+
+
+                </p>
+
+
 
             </div><br><br><br>
-            <div class="text-center">
-                <a href="<?= site_url('DpiController/melancolico/' . $Candidato->DPI); ?>" class="btn btn-success">
-                    <?php echo $Formulario->P1; ?>
-                </a>
+            <h2 style="text-align: center;">
+                Llevas
+                <?php echo $Formulario->idRespuesta; ?> de preguntas de 40:
+            </h2>
+            <?php
+            $valor_P1 = $Formulario->P1;
+            $valor_P2 = $Formulario->P2;
+            $valor_P3 = $Formulario->P3;
+            $valor_P4 = $Formulario->P4;
 
-                <a href="<?= site_url('DpiController/colerico/' . $Candidato->DPI); ?>" class="btn btn-success">
-                    <?php echo $Formulario->P2; ?>
-                </a>
+            $imagen_src1 = $valor_P1 . '.jpg'; // Concatena el valor de P1 con la extensión del archivo de imagen
+            $imagen_src2 = $valor_P2 . '.jpg'; // Concatena el valor de P1 con la extensión del archivo de imagen
+            $imagen_src3 = $valor_P3 . '.jpg'; // Concatena el valor de P1 con la extensión del archivo de imagen
+            $imagen_src4 = $valor_P4 . '.jpg'; // Concatena el valor de P1 con la extensión del archivo de imagen
+            
+            ?>
+            <table class="table">
+                <tr>
+                    <td style="text-align: center;">
+                        <div style="display: flex; flex-direction: column; align-items: center;">
+                            <a href="<?= site_url('DpiController/melancolico/' . $Candidato->DPI); ?>"
+                                class="btn btn-success">
+                                <?php echo $Formulario->P1; ?>
+                            </a>
+                            <img src="<?php echo base_url('assets/temperamentos/' . $imagen_src1); ?>" class="d-block"
+                                alt="Imagen" style="width: 70%; height: auto; margin-top: 10px;">
 
-                <a href="<?= site_url('DpiController/flematico/' . $Candidato->DPI); ?>" class="btn btn-success">
-                    <?php echo $Formulario->P3; ?>
-                </a>
+                        </div>
+                    </td>
+                    <td style="text-align: center;">
+                        <div style="display: flex; flex-direction: column; align-items: center;">
+                            <a href="<?= site_url('DpiController/melancolico/' . $Candidato->DPI); ?>"
+                                class="btn btn-success">
+                                <?php echo $Formulario->P2; ?>
+                            </a>
+                            <img src="<?php echo base_url('assets/temperamentos/' . $imagen_src2); ?>" class="d-block"
+                                alt="Imagen" style="width: 70%; height: auto; margin-top: 10px;">
 
-                <a href="<?= site_url('DpiController/sanguineo/' . $Candidato->DPI); ?>" class="btn btn-success">
-                    <?php echo $Formulario->P4; ?>
-                </a>
-            </div>
+                        </div>
+                    </td>
+                    <td style="text-align: center;">
+                        <div style="display: flex; flex-direction: column; align-items: center;">
+                            <a href="<?= site_url('DpiController/melancolico/' . $Candidato->DPI); ?>"
+                                class="btn btn-success">
+                                <?php echo $Formulario->P3; ?>
+                            </a>
+                            <img src="<?php echo base_url('assets/temperamentos/' . $imagen_src3); ?>" class="d-block"
+                                alt="Imagen" style="width: 70%; height: auto; margin-top: 10px;">
+
+                        </div>
+                    </td>
+                    <td style="text-align: center;">
+                        <div style="display: flex; flex-direction: column; align-items: center;">
+                            <a href="<?= site_url('DpiController/melancolico/' . $Candidato->DPI); ?>"
+                                class="btn btn-success">
+                                <?php echo $Formulario->P4; ?>
+                            </a>
+                            <img src="<?php echo base_url('assets/temperamentos/' . $imagen_src4); ?>" class="d-block"
+                                alt="Imagen" style="width: 70%; height: auto; margin-top: 10px;">
+
+                        </div>
+                    </td>
+                </tr>
+            </table>
+
 
             <!-- Agrega un campo oculto para almacenar la opción seleccionada -->
 
